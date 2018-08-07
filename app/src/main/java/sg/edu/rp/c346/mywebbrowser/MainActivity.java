7c1ae1902c3f7db39c937f102a5e76813b8badff
NUR NAJIHAH BTE NORDEEN <17010475@rp.edu.sg>
2018-08-07 11:58:35 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/mywebbrowser/MainActivity.java
@@ -0,0 +1,38 @@
+package sg.edu.rp.c346.mywebbrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLoadURL;
+    WebView wvMyPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLoadURL = findViewById(R.id.buttonLoad);
+        wvMyPage = findViewById(R.id.webViewMyPage);
+
+        wvMyPage.setWebViewClient(new WebViewClient());
+
+
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.google.com.sg";
+                wvMyPage.loadUrl(url);
+            }
+        });
+
+        wvMyPage.getSettings().setJavaScriptEnabled(true);
+        wvMyPage.getSettings().setAllowContentAccess(false);
+        wvMyPage.getSettings().setBuiltInZoomControls(true);
+
+    }
+}
